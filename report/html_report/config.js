253e852a244042468ef5780405a261405e2fc9d3
Yazheviks <vicsergeyeva@gmail.com>
2020-08-27 16:31:16 +0300

    make build

---
+++ report/html_report/config.js
@@ -0,0 +1,166 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_1024px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Entire_document_1024px.png",
+        "selector": "document",
+        "fileName": "Entire_document_1024px.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -792,
+            "height": 20
+          },
+          "misMatchPercentage": "22.57",
+          "analysisTime": 66
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Entire_document_1024px.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Entire_document_720px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Entire_document_720px.png",
+        "selector": "document",
+        "fileName": "Entire_document_720px.png",
+        "label": "Entire document",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720px",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -488,
+            "height": 20
+          },
+          "misMatchPercentage": "32.10",
+          "analysisTime": 72
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Entire_document_720px.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_1024px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Big_search_bar_data-qa_big_1024px.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "Big_search_bar_data-qa_big_1024px.png",
+        "label": "Big search bar data-qa_big",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -776,
+            "height": 110
+          },
+          "misMatchPercentage": "22.93",
+          "analysisTime": 78
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Big_search_bar_data-qa_big_1024px.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Big_search_bar_data-qa_big_720px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Big_search_bar_data-qa_big_720px.png",
+        "selector": "[data-qa=\"big\"]",
+        "fileName": "Big_search_bar_data-qa_big_720px.png",
+        "label": "Big search bar data-qa_big",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720px",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -472,
+            "height": 110
+          },
+          "misMatchPercentage": "32.84",
+          "analysisTime": 89
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Big_search_bar_data-qa_big_720px.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_1024px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Small_search_bar_data-qa_small_1024px.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "Small_search_bar_data-qa_small_1024px.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "1024px",
+        "engineErrorMsg": "Navigation failed because browser has disconnected!",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 776,
+            "height": -138
+          },
+          "misMatchPercentage": "22.94",
+          "analysisTime": 74
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Small_search_bar_data-qa_small_1024px.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Small_search_bar_data-qa_small_720px.png",
+        "test": "..\\bitmaps_test\\20200827-162915\\Small_search_bar_data-qa_small_720px.png",
+        "selector": "[data-qa=\"small\"]",
+        "fileName": "Small_search_bar_data-qa_small_720px.png",
+        "label": "Small search bar data-qa_small",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/search-bar-airbnb/",
+        "expect": 0,
+        "viewportLabel": "720px",
+        "engineErrorMsg": "net::ERR_CONNECTION_REFUSED at http://localhost:8080/index.html",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 472,
+            "height": -138
+          },
+          "misMatchPercentage": "23.18",
+          "analysisTime": 58
+        },
+        "diffImage": "..\\bitmaps_test\\20200827-162915\\failed_diff_Small_search_bar_data-qa_small_720px.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
